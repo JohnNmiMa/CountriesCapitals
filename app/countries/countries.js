d6e67bfb11f66a46a35f8f38f65e797cf65a8746
John <johnmarksjr@gmail.com>
2014-08-27 18:55:36 -0600

    Used ng-style to size the population and area divs - COOL!
    This cleaned up a lot of code, and forced my to improve the CSS.

--- app/countries/countries.js
+++ app/countries/countries.js
@@ -12,88 +12,49 @@ viewsModule.config(['$routeProvider', function($routeProvider) {
 
 viewsModule.controller('CountriesCtrl', ['$scope', 'countries',
                                  function($scope,   countries) {
-    var maxPopulation = pop = 0,
-        maxArea = area = 0;
-    $scope.countryNames = [];
+    var area = population = usIndex = 0;
 
-    for (country in countries) {
-        population = Number(countries[country].population);
-        area = Number(countries[country].areaInSqKm);
+    $scope.countries = countries;
+    $scope.maxPopulation = 0;
+    $scope.maxArea = 0;
 
-        if (maxPopulation < population) {
-            maxPopulation =  population;
+    for (index in countries) {
+        population = Number(countries[index].population);
+        area = Number(countries[index].areaInSqKm);
+
+        if ($scope.maxPopulation < population) {
+            $scope.maxPopulation =  population;
         }
-        if (maxArea < area) {
-            maxArea = area;
+        if ($scope.maxArea < area) {
+            $scope.maxArea = area;
         }
-
-        $scope.countryNames.push(countries[country].countryName);
+        if (countries[index].countryCode == "US") {
+            usIndex = index;
+        }
     }
 
     $scope.displayCountry = function(index) {
-        //console.log("Display " + $scope.countryNames[index]);
         setCountry(index);
     }
 
     function setCountry(index) {
-        var popWidth = Number($(".infograph .populationContainer").width()),
-            areaWidth = Number($(".infograph .areaContainer").width()),
-            popRadius = areaRadius = 0,
-            radiusText = "",
-            populationRatio = areaRatio = 1.0;
+        var width = Number($(".infograph .populationContainer").width());
+        $(".infograph .populationContainer").height(width);
+        $(".infograph .areaContainer").height(width);
 
-        // Update data bindings
         $scope.country = countries[index].countryName;
         $scope.population = Number(countries[index].population);
         $scope.area = Number(countries[index].areaInSqKm);
         $scope.capital = countries[index].capital;
-        $scope.capitalPopulation = "???";
-
-        // Compute population infographic size
-        computePopulationLayout($scope.population, popWidth);
-        computeAreaLayout($scope.area, popWidth);
-
     }
 
-    function computePopulationLayout(countryPopulation, popContainerWidth) {
-        var populationRatio = countryPopulation / maxPopulation,
-            popWidth = popContainerWidth * populationRatio,
-            radiusText = "",
-            horzOffset = 0;
-
-        if (popWidth < 2.0) {
-            popWidth = 2;
-        }
-        radiusText = (popWidth/2).toString() + "px";
-        $(".infograph .populationContainer").height(popWidth);
-        $(".infograph .population").width(popWidth);
-        $(".infograph .population").height(popWidth);
-        $(".infograph .population").css('border-radius',radiusText);
-
-        horzOffset = (popContainerWidth - popWidth) / 2;
-        $(".infograph .population").css('left',horzOffset);
-        $(".infograph .population").css('top',horzOffset);
+    $scope.computeLayout = function(size, maxSize) {
+        var ratio = ((size/maxSize) < 0.01) ? 0.01 : size/maxSize,
+            widthPct = ratio * 100,
+            widthPctT = widthPct.toString() + "%",
+            offsetPctT = ((100 - widthPct)/2).toString() + "%";
+        return {'width':widthPctT, 'height':widthPctT, 'border-radius':'50%', 'left':offsetPctT, 'top':offsetPctT};
     }
 
-    function computeAreaLayout(countryArea, areaContainerWidth) {
-        var areaRatio = countryArea / maxArea,
-            areaWidth = areaContainerWidth * areaRatio,
-            radiusText = "",
-            horzOffset = 0;
-
-        if (areaWidth < 2.0) {
-            areaWidth = 2;
-        }
-        radiusText = (areaWidth/2).toString() + "px";
-        $(".infograph .areaContainer").height(areaWidth);
-        $(".infograph .area").width(areaWidth);
-        $(".infograph .area").height(areaWidth);
-        $(".infograph .area").css('border-radius',radiusText);
-
-        horzOffset = (areaContainerWidth - areaWidth) / 2;
-        $(".infograph .area").css('left',horzOffset);
-        $(".infograph .area").css('top',horzOffset);
-    }
-
-    setCountry(0);
+    setCountry(usIndex);
 }]);
