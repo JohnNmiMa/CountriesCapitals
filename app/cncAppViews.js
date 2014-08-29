7f450464858a1ed9fa621b49b175d52fc36c5ad5
John <johnmarksjr@gmail.com>
2014-08-29 17:04:12 -0600

    Implemented ng-view animation

--- app/cncAppViews.js
+++ app/cncAppViews.js
@@ -1,2 +1,2 @@
-var viewsModule = angular.module('cncAppViews', ['ngRoute', 'cncLibrary'])
+var viewsModule = angular.module('cncAppViews', ['ngRoute', 'ngAnimate', 'cncLibrary'])
 .constant('SQKM_TO_SQMI', 0.386102);
