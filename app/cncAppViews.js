21e056277b3ec4c791f68220d8073504581cee15
John <johnmarksjr@gmail.com>
2014-08-29 20:44:04 -0600

    Refactored all routes into cncAppViews.js

--- app/cncAppViews.js
+++ app/cncAppViews.js
@@ -1,2 +1,30 @@
 var viewsModule = angular.module('cncAppViews', ['ngRoute', 'ngAnimate', 'cncLibrary'])
-.constant('SQKM_TO_SQMI', 0.386102);
+
+.constant('SQKM_TO_SQMI', 0.386102)
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl : "./home/home.html",
+        controller : 'HomeCtrl'
+    })
+    .when("/countries/:country/capital", {
+        templateUrl : "./country/country.html",
+        controller : 'CountryCtrl',
+        resolve : {
+            country : ['cncCountry', '$route', function(cncCountry, $route) {
+                var countryCode = $route.current.params.country;
+                return cncCountry(countryCode);
+            }]
+        }
+    })
+    .when("/countries", {
+        templateUrl : "./countries/countries.html",
+        controller : 'CountriesCtrl',
+        resolve : {
+            countries : ['cncCountries', function(cncCountries) {
+                return cncCountries();
+            }]
+        }
+    });
+}]);
+
