5d89a30e8cf3ba2a85a1a365857631f1950cdbdb
John <johnmarksjr@gmail.com>
2014-09-09 17:32:13 -0600

    Added null route

--- app/cncRoutesSpec.js
+++ app/cncRoutesSpec.js
@@ -1,7 +1,7 @@
 describe('cncAppViews', function() {
     beforeEach(angular.mock.module('cncAppViews'));
 
-    it('should map /countries route to its controller', inject(function($route) {
+    it('should map /countries route to /countries/ALL', inject(function($route) {
         expect($route.routes['/countries'].redirectTo()).toBe('/countries/ALL');
     }));
 
@@ -13,5 +13,13 @@ describe('cncAppViews', function() {
     it('should map /countries/:country/capital route to its controller', inject(function($route) {
         expect($route.routes['/countries/:country/capital'].controller).toBe('CapitalCtrl');
         expect($route.routes['/countries/:country/capital'].templateUrl).toEqual('./capital/capital.html');
+    }));
+});
+
+describe('cncApp', function() {
+    beforeEach(angular.mock.module('cncApp'));
+
+    it('should map /unexpected route to controller /', inject(function($route) {
+        expect($route.routes[null].redirectTo).toBe('/');
     }));
 });
